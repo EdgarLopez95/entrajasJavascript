a7370a3cfc8d8da735358be1f8b68f1c1a18d75c
Edgar Felipe <edgar_felipe13@hotmail.com>
2022-10-22 01:12:47 -0500

    usando fech

--- entregasCoder/index.js
+++ entregasCoder/index.js
@@ -1,119 +1,79 @@
-let option;
-const tranins = [
-  {
-    train: "mancuernas de pie",
-    bodyPart: "BRAZO",
-    difficulty: "MEDIO",
-  },
-];
+const getData = async () => {
+  const response = await fetch("./data.json");
+  const data = await response.json();
+  console.log(data);
+  //return data;
 
-function agregar() {
-  const agregarEjercicios = document.getElementById("cambiarSection");
-  agregarEjercicios.innerHTML = `
-  <h2>Por favor selecciona el entreno</h2>
-  <button id="agregarPecho">PECHO</button>
-  <button id="agregarBrazo">BRAZO</button>
-  <button id="agregarPierna">PIERNA</button>
-  <button id="agregarEspalda">ESPALDA</button>
-  <button id="agregarHombro">HOMBRO</button>
-  <button id="agregarAbdominales">ABDOMINALES</button>
-  <button id="agregarCardio">CARDIO</button>
+  // muestra el detalle de ese ejercicio en esa pantalla
+  function mostrarDetalle(ejercicio) {
+    const contenedorDeEjercicios = document.getElementById(
+      "contenedor-de-ejercicios"
+    );
+    contenedorDeEjercicios.innerHTML = "";
+
+    contenedorDeEjercicios.innerHTML = `
+  <img src="${ejercicio.imagen}" alt="${ejercicio.titulo}">
+  <h3>${ejercicio.titulo}</h3>
+  <p> ${ejercicio.parteDelCuerpo}</p>
+  <p> ${ejercicio.difucultad}</p>
+  <a href="${ejercicio.link}" target="_blank">Ver Trailer</a>
   `;
-  (" <h2>Por favor selecciona el entreno</h2> <button>PECHO</button> <button>BRAZO</button> <button>PIERNA</button> <button>ESPALDA</button>");
-
-  const PECHO = document.getElementById("agregarPecho");
-  PECHO.addEventListener("click", () => {
-    const agregarEjercicios = document.getElementById("cambiarSection");
-    agregarEjercicios.innerHTML = `
-  <h2>Por favor escribe el ejercicio y la dificultad</h2>
-  <form action="">
-  <label for="inputTrain">
-    Escribe el ejercicio
-  </label>
-  <input
-    type="text"
-    id="inputTrain"
-    placeholder="press plano"
-  />
-  <br>
-    <label for="inputdif">
-    Pon la difucultad
-  </label>
-  <input
-    type="text"
-    id="inputdif"
-    placeholder="press plano"
-  />
-  <button type="button" id="botonDatos">Agregar</button>
-  `;
-    let botonDatos = document.getElementById("botonDatos");
-    botonDatos.onclick = () => {
-      const inputTrain = document.getElementById("inputTrain");
-      const inputHard = document.getElementById("inputdif");
-      const valTrain = inputTrain.value;
-      const valtHard = inputHard.value;
-
-      createTrain("PECHO", valTrain, valtHard);
-      ver();
-    };
-  });
-}
-
-function ver() {
-  const mostrarEjercicios = document.getElementById("cambiarSection");
-  mostrarEjercicios.innerHTML = ""
-  tranins.forEach((train) => {
-    const sectionEntrenos = document.createElement("section");
-    sectionEntrenos.innerHTML = `
-    <h2>Este es un entreno</h2>
-    <p">Tu entrenador te ha asignado un ejercicio de:</p>
-    <h3>"${train.bodyPart}"</h3>
-    <p">El ejercicio que vas a hacer es:</p>
-    <h3>"${train.train}"</h3>
-    <p">su dificultad es:</p>
-    <h3>"${train.difficulty}"</h3>
-    `;
-    mostrarEjercicios.appendChild(sectionEntrenos);
-  });
-  //acá guardo en el local storage
-  const guardarLocal = (clave, valor) => {
-    localStorage.setItem(clave, valor);
-  };
-  for (const train of tranins) {
-    guardarLocal("entreno", JSON.stringify(train));
+    crearBotonVolver();
   }
-  //console.log(tranins);
-}
-function salir() {
-  Toastify({
-    text: "Usted salio de la app",
-    duration: 3000,
-    gravity: "bottom", // `top` or `bottom`
-    position: "center", // `left`, `center` or `right`
-    //stopOnFocus: true, // Prevents dismissing of toast on hover
-    style: {
-      background: "linear-gradient(to right, #c29968, #c29968)",
-    },
-  }).showToast();
-  let agregarEjercicios = document.getElementById("cambiarSection");
-  agregarEjercicios.innerHTML = `
-  <h2>USTED SALIO DE LA APP</h2>
-  `;
-}
+  // crea el boton de regresar en el detalle
+  function crearBotonVolver() {
+    const contenedorDeEjercicios = document.getElementById(
+      "contenedor-de-ejercicios"
+    );
+    const botonVolver = document.createElement("button");
+    botonVolver.innerText = "volver";
+    botonVolver.addEventListener("click", () => {
+      mostrarEjercicios(data);
+    });
+    contenedorDeEjercicios.append(botonVolver);
+  }
+  // crea boton ver detalle
+  function crearBotonVerDetalle(ejercicio) {
+    const button = document.createElement("button");
+    button.innerText = "Ver Detalle";
+    button.addEventListener("click", () => {
+      mostrarDetalle(ejercicio);
+      crearBotonVolver();
+    });
+    return button;
+  }
 
-function createTrain(bodyPart, train, difficulty) {
-  tranins.push({
-    bodyPart,
-    train,
-    difficulty,
-  });
-}
+  //muestra los ejercicios
+  function mostrarEjercicios(ejercicios) {
+    const contenedorDeEjercicio = document.getElementById(
+      "contenedor-de-ejercicios"
+    );
+    contenedorDeEjercicio.innerHTML = "";
 
-function getAllTrains() {}
+    ejercicios.forEach((ejercicio) => {
+      const divEjercicio = document.createElement("div");
 
-function getLastId() {
-  return;
-  //return tranins.length;
-}
+      divEjercicio.innerHTML = `
+    <img src="${ejercicio.imagen}" alt="${ejercicio.titulo}">
+    <h3>${ejercicio.titulo}</h3>
+    <p> ${ejercicio.parteDelCuerpo}</p>
+    <p> ${ejercicio.difucultad}</p>
+    <a href="${ejercicio.link}" target="_blank">Ver Ejemplo</a>
+    `;
+      const button = document.createElement("button");
+      divEjercicio.append(button);
+      button.innerText = "Ver Detalle";
+      button.addEventListener("click", () => {
+        mostrarDetalle(ejercicio);
+      });
 
-console.log(tranins);
+      console.log(crearBotonVerDetalle(ejercicio));
+
+      contenedorDeEjercicio.appendChild(divEjercicio);
+    });
+  }
+
+  mostrarEjercicios(data);
+};
+
+getData();
